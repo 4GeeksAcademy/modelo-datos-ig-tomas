98683d477fb22804f0cf3447b653f688bec7de52
TomasUhiaOtero <tomasuhiaotero@gmail.com>
2025-08-19 15:50:12 +0000

    Claves foraneas

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -8,7 +8,10 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    __tablename__ = "User"
+
     id: Mapped[int] = mapped_column(primary_key=True)
+    nick: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
@@ -17,18 +20,24 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "nick": self.nick,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active
+
         }
 
 # Modelo post
 
 
 class Post(db.Model):
+    __tablename__ = "Post"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    user: Mapped[str] = mapped_column(foreign_key=True)
     date: Mapped[str] = mapped_column(String(8), unique=False, nullable=False)
     caption: Mapped[str] = mapped_column(String(120), nullable=True)
+
+    user_id: Mapped[str] = mapped_column(ForeignKey("User.id"), nullable=False)
+    user: Mapped[list["User"]] = relationShip(back_populate="Post")
 
     def serialize(self):
         return {
@@ -42,10 +51,14 @@ class Post(db.Model):
 
 
 class Coment(db.Model):
+    __tablename__ = "Coment"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    user: Mapped[str] = mapped_column(foreign_key=True)
     text: Mapped[str] = mapped_column(String(120), unique=False, nullable=True)
     like: Mapped[bool] = mapped_column(Boolean, nullable=True)
+
+    user_id: Mapped[str] = mapped_column(ForeignKey("User.id"), nullable=False)
+    user: Mapped[list["User"]] = relationShip(back_populate="Coment")
 
     def serialize(self):
         return {
@@ -59,13 +72,35 @@ class Coment(db.Model):
 
 
 class Story(db.Model):
+    __tablename__ = "Story"
     id: Mapped[int] = mapped_column(primary_key=True)
-    user: Mapped[str] = mapped_column(foreign_key=True)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    like: Mapped[bool] = mapped_column(Boolean, nullable=True)
+
+    user_id: Mapped[str] = mapped_column(ForeignKey("User.id"), nullable=False)
+    user: Mapped[list["User"]] = relationShip(back_populate="Story")
 
     def serialize(self):
         return {
             "id": self.id,
             "user": self.user,
-            "is_active": self.is_active
+            "is_active": self.is_active,
+            "like": self.like
+        }
+
+# Modelo follower
+
+
+class Follower(db.Model):
+    __tablename__ = "Follower"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[str] = mapped_column(ForeignKey("User.id"), nullable=False)
+    user: Mapped[list["User"]] = relationShip(back_populate="Follower")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user,
         }
